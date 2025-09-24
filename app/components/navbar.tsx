b8a81bd4387e90eb2affeb1f04fcea7ce9cc92d3
alimuhammed_Dev <muhammedaliali2020@gmail.com>
2025-09-24 23:38:43 +0100

    worked on latest events

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -8,7 +8,7 @@ export const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="bg-white shadow-md">
+    <nav className="bg-white shadow-md fixed top-0 left-0 right-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="flex-shrink-0">
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import { Navbar } from "./components/navbar";
 import { useState, useEffect } from "react";
+import { Calendar, MapPin } from "lucide-react";
 
 export default function Home() {
   const images = [
@@ -30,7 +31,7 @@ export default function Home() {
         <Navbar />
       </div>
 
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 lg:mt-20 mt-7 lg:bg-[#17364A] lg:p-20 rounded-4xl">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 lg:mt-28 mt-24 lg:bg-[#17364A] lg:p-20 rounded-4xl mb-20">
         <div className="flex lg:flex-row flex-col items-center justify-between gap-10">
           <div>
             <h1 className="lg:text-4xl text-2xl font-medium lg:text-white">
@@ -55,6 +56,211 @@ export default function Home() {
               height={450}
               className="rounded-lg transition-all duration-700 ease-in-out"
             />
+          </div>
+        </div>
+      </div>
+
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 lg:mt-20 mt-7">
+        <h1 className="text-[#17364A] lg:text-4xl text-2xl font-medium">
+          Latest Events
+        </h1>
+        <div className="grid lg:grid-cols-3 grid-cols-1 gap-10 mt-5 mb-20">
+          <div className="max-w-sm rounded-xl overflow-hidden shadow-lg bg-white hover:shadow-xl transition duration-300">
+            <Image
+              src="/events1.jpg"
+              alt="event"
+              width={350}
+              height={350}
+              className="w-full h-60 object-cover"
+            />
+            <div className="p-4">
+              <h1 className="lg:text-xl text-lg font-semibold text-gray-800">
+                BTS Super Home Coming Show
+              </h1>
+              <p className="text-gray-600 mt-2 flex items-center gap-1">
+                <Calendar className="w-4 h-4" /> 01 December 2025
+              </p>
+              <p className="text-gray-600 flex items-center gap-1">
+                <MapPin className="w-4 h-4" /> Korea Show Arena, Korea 
+              </p>
+              <button className="bg-[#17364A] text-white px-6 py-3 rounded-full mt-5 font-medium hover:bg-gray-800 transition w-full">
+                Book Now
+              </button>
+            </div>
+          </div>
+          <div className="max-w-sm rounded-xl overflow-hidden shadow-lg bg-white hover:shadow-xl transition duration-300">
+            <Image
+              src="/events4.jpg"
+              alt="event"
+              width={350}
+              height={350}
+              className="w-full h-60 object-cover"
+            />
+            <div className="p-4">
+              <h1 className="lg:text-xl text-lg font-semibold text-gray-800">
+                Coffee Shop Show
+              </h1>
+              <p className="text-gray-600 mt-2 flex items-center gap-1">
+                <Calendar className="w-4 h-4" /> 01 October 2025
+              </p>
+              <p className="text-gray-600 flex items-center gap-1">
+                <MapPin className="w-4 h-4" /> Primes Mart Coffee Shop, UK
+              </p>
+              <button className="bg-[#17364A] text-white px-6 py-3 rounded-full mt-5 font-medium hover:bg-gray-800 transition w-full">
+                Book Now
+              </button>
+            </div>
+          </div>
+          <div className="max-w-sm rounded-xl overflow-hidden shadow-lg bg-white hover:shadow-xl transition duration-300">
+            <Image
+              src="/events5.jpg"
+              alt="event"
+              width={350}
+              height={350}
+              className="w-full h-60 object-cover"
+            />
+            <div className="p-4">
+              <h1 className="lg:text-xl text-lg font-semibold text-gray-800">
+                Milly and Albert Exclusive Wedding
+              </h1>
+              <p className="text-gray-600 mt-2 flex items-center gap-1">
+                <Calendar className="w-4 h-4" /> 01 November 2025
+              </p>
+              <p className="text-gray-600 flex items-center gap-1">
+                <MapPin className="w-4 h-4" /> Grand Royal Palace, New York
+              </p>
+              <button className="bg-[#17364A] text-white px-6 py-3 rounded-full mt-5 font-medium hover:bg-gray-800 transition w-full">
+                Book Now
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 lg:mt-20 mt-7">
+        <h1 className="text-[#17364A] lg:text-4xl text-2xl font-medium">
+          Past Events
+        </h1>
+        <div className="grid lg:grid-cols-3 grid-cols-1 gap-10 mt-5 mb-20">
+          <div className="max-w-sm rounded-xl overflow-hidden shadow-lg bg-white hover:shadow-xl transition duration-300">
+            <Image
+              src="/events2.jpg"
+              alt="event"
+              width={350}
+              height={350}
+              className="w-full h-60 object-cover"
+            />
+            <div className="p-4">
+              <h1 className="lg:text-xl text-lg font-semibold text-gray-800">
+                Coldplay Show, New Jersey
+              </h1>
+              <p className="text-gray-600 mt-2 flex items-center gap-1">
+                <Calendar className="w-4 h-4" /> 01 August 2025
+              </p>
+              <p className="text-gray-600 flex items-center gap-1">
+                <MapPin className="w-4 h-4" /> New Jersey Stadium Arena 
+              </p>
+            </div>
+          </div>
+          <div className="max-w-sm rounded-xl overflow-hidden shadow-lg bg-white hover:shadow-xl transition duration-300">
+            <Image
+              src="/events3.jpg"
+              alt="event"
+              width={350}
+              height={350}
+              className="w-full h-60 object-cover"
+            />
+            <div className="p-4">
+              <h1 className="lg:text-xl text-lg font-semibold text-gray-800">
+                Brooklyn Business Seminar
+              </h1>
+              <p className="text-gray-600 mt-2 flex items-center gap-1">
+                <Calendar className="w-4 h-4" /> 10 August 2025
+              </p>
+              <p className="text-gray-600 flex items-center gap-1">
+                <MapPin className="w-4 h-4" /> Scale Arena, Booklyn, NY
+              </p>
+            </div>
+          </div>
+          <div className="max-w-sm rounded-xl overflow-hidden shadow-lg bg-white hover:shadow-xl transition duration-300">
+            <Image
+              src="/events6.jpg"
+              alt="event"
+              width={350}
+              height={350}
+              className="w-full h-60 object-cover"
+            />
+            <div className="p-4">
+              <h1 className="lg:text-xl text-lg font-semibold text-gray-800">
+                Chicago High School Reunion
+              </h1>
+              <p className="text-gray-600 mt-2 flex items-center gap-1">
+                <Calendar className="w-4 h-4" /> 28 July 2025
+              </p>
+              <p className="text-gray-600 flex items-center gap-1">
+                <MapPin className="w-4 h-4" /> Malleys Place, Chicago
+              </p>
+            </div>
+          </div>
+          <div className="max-w-sm rounded-xl overflow-hidden shadow-lg bg-white hover:shadow-xl transition duration-300">
+            <Image
+              src="/events7.jpg"
+              alt="event"
+              width={350}
+              height={350}
+              className="w-full h-60 object-cover"
+            />
+            <div className="p-4">
+              <h1 className="lg:text-xl text-lg font-semibold text-gray-800">
+                Milly and Albert Exclusive Wedding
+              </h1>
+              <p className="text-gray-600 mt-2 flex items-center gap-1">
+                <Calendar className="w-4 h-4" /> 01 August 2025
+              </p>
+              <p className="text-gray-600 flex items-center gap-1">
+                <MapPin className="w-4 h-4" /> Grand Royal Palace, New York
+              </p>
+            </div>
+          </div>
+          <div className="max-w-sm rounded-xl overflow-hidden shadow-lg bg-white hover:shadow-xl transition duration-300">
+            <Image
+              src="/events8.jpg"
+              alt="event"
+              width={350}
+              height={350}
+              className="w-full h-60 object-cover"
+            />
+            <div className="p-4">
+              <h1 className="lg:text-xl text-lg font-semibold text-gray-800">
+                Milly and Albert Exclusive Wedding
+              </h1>
+              <p className="text-gray-600 mt-2 flex items-center gap-1">
+                <Calendar className="w-4 h-4" /> 01 August 2025
+              </p>
+              <p className="text-gray-600 flex items-center gap-1">
+                <MapPin className="w-4 h-4" /> Grand Royal Palace, New York
+              </p>
+            </div>
+          </div>
+          <div className="max-w-sm rounded-xl overflow-hidden shadow-lg bg-white hover:shadow-xl transition duration-300">
+            <Image
+              src="/events9.jpg"
+              alt="event"
+              width={350}
+              height={350}
+              className="w-full h-60 object-cover"
+            />
+            <div className="p-4">
+              <h1 className="lg:text-xl text-lg font-semibold text-gray-800">
+                Milly and Albert Exclusive Wedding
+              </h1>
+              <p className="text-gray-600 mt-2 flex items-center gap-1">
+                <Calendar className="w-4 h-4" /> 01 August 2025
+              </p>
+              <p className="text-gray-600 flex items-center gap-1">
+                <MapPin className="w-4 h-4" /> Grand Royal Palace, New York
+              </p>
+            </div>
           </div>
         </div>
       </div>
